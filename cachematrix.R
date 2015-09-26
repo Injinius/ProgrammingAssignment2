d6b8eae8bad63d435de2b3f57e9a0799d028c024
Injinius <resendes@yahoo.com>
2015-09-25 22:37:58 -0400

    Initial commit for Programming Assignment 2
    Initial implementation with basic documentation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions that aid in caching a matrix and its inverse
 
-## Write a short comment describing this function
+## makeCacheMatrix
+## makes and returns an object that:
+## has a set/get functions to set/get the provided matrix
+## has setinverse/getinverse functions to return the matrix's inverse
+##
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(i) inverse <<- i
+  getinverse <- function() inverse 
+  list(set = set, 
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve
+## Returns the inverse (via solve()) of the provided matrix.
+## If the inverse was previously cached, then return the cached value.
+##
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Assumption: x is invertible
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  m <- x$get()
+  i <- solve(m, ...)
+  x$setinverse(i)
+  i
 }
+
+##
+## Sample run
+##
+# > bbb<-matrix(c(1, 3, 3, 1, 4, 3, 1, 3, 4), 3, 3, byrow=TRUE)
+# > cbbb <- makeCacheMatrix(bbb)
+# > cbbb$get()
+#      [,1] [,2] [,3]
+# [1,]    1    3    3
+# [2,]    1    4    3
+# [3,]    1    3    4
+#
+# > cacheSolve(cbbb)
+#     [,1] [,2] [,3]
+# [1,]    7   -3   -3
+# [2,]   -1    1    0
+# [3,]   -1    0    1
+#
+# > cacheSolve(cbbb)
+# getting cached data
+#      [,1] [,2] [,3]
+# [1,]    7   -3   -3
+# [2,]   -1    1    0
+# [3,]   -1    0    1
